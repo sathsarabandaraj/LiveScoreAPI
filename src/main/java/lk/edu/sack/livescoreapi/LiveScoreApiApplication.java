9ac724118f95597c363f54cc973f7df9a16141fe
Sathsara Bandara Jayasundara <sathsarabjayasundara@gmail.com>
2024-01-17 22:10:25 +0530

    Initiate Project

---
+++ src/main/java/lk/edu/sack/livescoreapi/LiveScoreApiApplication.java
@@ -0,0 +1,13 @@
+package lk.edu.sack.livescoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveScoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveScoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/edu/sack/livescoreapi/LiveScoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.edu.sack.livescoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveScoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
